5211bc0469067c8f2eaa566c926f15bc1bbc1838
SnirNahum <snir.nahum@playermaker.com>
2022-02-25 17:06:52 +0200

    copied code

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,40 +1,40 @@
-import config from "../config/index";
+import config from '../config/index';
 
 async function register(user) {
-  const res = await fetch(config.apiUrl + "/user", {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  return res.json();
+	const res = await fetch(config.apiUrl + '/user', {
+		method: 'POST',
+		body: JSON.stringify(user),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	return res.json();
 }
 
 async function login({ username, password }) {
-  const res = await fetch(config.apiUrl + "/login", {
-    method: 'POST',
-    body: JSON.stringify({ username, password }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return res.json();
+	const res = await fetch(config.apiUrl + '/login', {
+		method: 'POST',
+		body: JSON.stringify({ username, password }),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+	return res.json();
 }
 
 async function me() {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    return {};
-  }
-  const res = await fetch(config.apiUrl + "/user/me", {
-    method: 'GET',
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": token
-    }
-  });
-  return res.json();
+	const token = localStorage.getItem('token');
+	if (!token) {
+		return {};
+	}
+	const res = await fetch(config.apiUrl + '/user/me', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': token
+		}
+	});
+	return res.json();
 }
 
 async function getUser(username) {
@@ -64,25 +64,24 @@ async function search(query) {
 }
 
 async function Follow(username) {
-    return fetch(config.apiUrl + '/user/' + username + '/Follow', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('token')
-      }
-    });
+	return fetch(config.apiUrl + '/user/' + username + '/follow', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': localStorage.getItem('token')
+		}
+	});
 }
 
 async function unFollow(username) {
-  return await fetch(config.apiUrl + '/user/' + username + '/unfollow', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': localStorage.getItem('token')
-    }
-  });
-  
+	return fetch(config.apiUrl + '/user/' + username + '/unfollow', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': localStorage.getItem('token')
+		}
+	});
 }
 
 
-export { register, login, me, getUser, search,Follow,unFollow };
+export { register, login, me, getUser, search, unFollow, Follow };
